4c19634ad7c49d1bfa13707992e4b88eec636a0b
Andrei Sousa <sousandrei@gmail.com>
2020-10-04 20:07:25 +0200

    :construction: Treating errors in api

--- src/api/list.rs
+++ src/api/list.rs
@@ -1,5 +1,6 @@
 use hyper::{Body, Response, StatusCode};
 
+use super::build_response;
 use crate::StatePtr;
 
 pub async fn handler(state_ptr: StatePtr) -> Result<Response<Body>, hyper::Error> {
@@ -7,8 +8,17 @@ pub async fn handler(state_ptr: StatePtr) -> Result<Response<Body>, hyper::Error
 
     let response_json = serde_json::json!(state.vms);
 
-    let body = serde_json::to_string_pretty(&response_json).unwrap();
+    let body = match serde_json::to_string_pretty(&response_json) {
+        Ok(b) => b,
+        Err(e) => {
+            let response = build_response(
+                StatusCode::OK,
+                format!("Error parsing state [{}]", e.to_string()),
+            );
+            return Ok(response);
+        }
+    };
 
-    let response = super::build_response(StatusCode::OK, body);
+    let response = build_response(StatusCode::OK, body);
     Ok(response)
 }
